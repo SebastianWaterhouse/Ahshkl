d2d4d330df499aa830e35e8ad3e69d5913bb1ceb
The Rambler <littlestllama6@gmail.com>
2015-02-18 11:27:03 -0600

    Check that atma function

--- Ahshkl-game/ahshklmain.py
+++ Ahshkl-game/ahshklmain.py
@@ -11,16 +11,19 @@ import time, sys
 
 bopa = bopas.bodyPart
 bopasubsnames = ([bopasubc.__name__ for bopasubc in vars()['bopa'].__subclasses__()])
+atma = atmas.MaterialAttributes
+atmasubsnames = ([atmasubc.__name__ for atmasubc in vars()['atma'].__subclasses__()])
 
 toCall = "ding-o"
 
-toCall = raw_input("Please enter class name of object to test (Help function activated by typing 'help'): ").lower()
+toCall = raw_input("Please enter class name of object to test followed by name of atma to use (Help function activated by typing 'help'): ").lower()
+toCall = toCall.split()
 if 1:
 	try:
-		if toCall == "help":
-			print("List of bopas are as follows: " + str(bopasubsnames))
+		if toCall[0] == "help":
+			print("List of bopas are as follows: " + str(bopasubsnames) + " and the atmas are: " + str(atmasubsnames))
 			sys.exit("Exited with code 0")
-		if toCall == "enterdebug":
+		if toCall[0] == "enterdebug":
 			toDo = raw_input("Welcome to debug. Please enter what you would like to do: ")
 			try:
 				print(eval(toDo))
@@ -29,16 +32,17 @@ if 1:
 				print("Error! " + toDo + " is not defined! Check your spelling and make sure it exists!")
 				sys.exit("Exited with code NameError")
 			except AttributeError:
-				print("Error! " + toDo + " appears to not exist! Check your spelling and make sure it exists!")
+				print("Error! " + toDo[0] + " appears to not be a class! Check your spelling and make sure it exists!")
 				sys.exit("Exited with code AttributeError")
-		calling = getattr(bopas, toCall)
+		calling = getattr(bopas, toCall[0])
+		callingatma = getattr(atmas, toCall[1])
 		print("Welcome to the debug object parser interface. This is a demo of a " + calling.shapename + ". Caps have been added to all fields for emphasis. In normal gameplay it is capitalized properly.")
-		calling.atma = atmas.attributeWood.adjective
-		sentence = ("There is a " + calling.sentencename.upper()  + " with codename of " + calling.codeName.upper() + ", a build ID of " + str(calling.bID) + "a size unit of " + str(calling.size_unit) + ", and an atma of " + calling.atma.upper())
-		time.sleep(4)
+		calling.atma = callingatma.adjective
+		sentence = ("There is a " + calling.sentencename.upper()  + " with codename of " + calling.codeName.upper() + ", a build ID of " + str(calling.bID) + ", a size unit of " + str(calling.size_unit) + ", and an atma of " + calling.atma.upper())
+		time.sleep(1.5)
 		print(sentence)
 	except AttributeError:
-		print("Error! Invalid class! Class '" + toCall + "' not found in file ahshklbodyparts.py")
+		print("Error! Invalid class! Class '" + toCall[0] + "' or " + toCall[1] + "not found in file ahshklbodyparts.py or attributesMaterial.py, respectively")
 		sys.exit("Exiting with code AttributeError")
 
 
--- Ahshkl-game/assets/bopas/attributes/attributesMaterial.py
+++ Ahshkl-game/assets/bopas/attributes/attributesMaterial.py
@@ -2,6 +2,12 @@ class MaterialAttributes(object):
 	adjective = "UNSET"
 	codeName = "UNSET"
 
-class attributeWood(MaterialAttributes):
+class attributewood(MaterialAttributes):
 	adjective = "wooden"
 	codeName = "attributeWooden"
+class attributestone(MaterialAttributes):
+	adjective = "stone"
+	codeName = "attributeStone"
+class attributedirt(MaterialAttributes):
+	adjective = "dirt"
+	codeName = "attributeDirt"
